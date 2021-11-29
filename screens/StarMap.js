45d67bdfd8b5dfe0b378cea5b29fbba641be8f2a
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-29 14:18:14 -0600

    Update StarMap.js

--- screens/StarMap.js
+++ screens/StarMap.js
@@ -10,7 +10,7 @@ export default class StarMapScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>StarMap Screen!</Text>
+                <Text>¡Pantalla de mapa estelar!</Text>
             </View>
         )
     }
