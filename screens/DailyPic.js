29f1b1410b2f41a5f5c55799746220491f7080ee
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-29 14:15:02 -0600

    Update DailyPic.js

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -10,7 +10,7 @@ export default class DailyPicScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>DailyPic Screen!</Text>
+                <Text>¡Pantalla de imágenes diarias!</Text>
             </View>
         )
     }
