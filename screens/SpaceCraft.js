cd766fe51e861eabd655ab3d565a24ebdc1585d9
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-29 14:17:41 -0600

    Update SpaceCraft.js

--- screens/SpaceCraft.js
+++ screens/SpaceCraft.js
@@ -10,7 +10,7 @@ export default class SpaceCraftScreen extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>SpaceCraft Screen!</Text>
+                <Text>¡Pantalla de naves espaciales!</Text>
             </View>
         )
     }
